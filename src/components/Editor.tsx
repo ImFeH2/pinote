026f92074b2bd749af5b4c4881872c51766f0e2e
ImFeH2 <i@feh2.im>
2026-02-15 18:45:28 +0800

    fix: add editor margin to prevent cursor overlap with window resize handles

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -89,7 +89,7 @@ function EditorInner({ defaultValue, onChange }: EditorInnerProps) {
 
   return (
     <div
-      className="flex min-h-0 flex-1 cursor-text flex-col"
+      className="mx-1 flex min-h-0 flex-1 cursor-text flex-col"
       onClick={handleContainerClick}
     >
       <Milkdown />
