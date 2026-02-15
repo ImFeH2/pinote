fdc938735f59104fd4117119a8ba52ab0f601dbd
ImFeH2 <i@feh2.im>
2026-02-15 14:55:31 +0800

    feat: initialize Tauri project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "@/styles/App.css";
+
+function App() {
+  return (
+    <div className="text-center text-3xl font-bold">
+      Welcome to Pinote!
+    </div>
+  );
+}
+
+export default App;
