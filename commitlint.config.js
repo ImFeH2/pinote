99c5bde0edf077edac48949e988939a98ed2b71a
ImFeH2 <i@feh2.im>
2026-09-09 23:22:13 +0800

    chore: organize quality checks and enforce commit rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,9 @@
 export default {
   extends: ["@commitlint/config-conventional"],
+  defaultIgnores: false,
+  rules: {
+    "body-empty": [2, "always"],
+    "footer-empty": [2, "always"],
+    "scope-empty": [2, "always"],
+  },
 };
